3c30607d44a5a836f80ba01c4cf0ef540d985269
Manish Kumar <dasmanish92@gmail.com>
2021-05-13 11:59:08 +0530

    JUnit Updated
    updated for JUnit

--- src/test/java/org/employee/management/EmployeeManagementApplicationTests.java
+++ src/test/java/org/employee/management/EmployeeManagementApplicationTests.java
@@ -1,50 +1,69 @@
-/*
 package org.employee.management;
 
-import com.sun.istack.NotNull;
 import org.employee.management.entities.Department;
 import org.employee.management.entities.Employee;
 import org.employee.management.entities.Salary;
 import org.employee.management.enums.Gender;
 import org.employee.management.repository.EmpRepository;
-//import org.junit.jupiter.api.Test;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.hamcrest.Matchers.*;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @SpringBootTest
 class EmployeeManagementApplicationTests {
 
-    @Autowired
-    EmpRepository empRepository;
+    @Mock
+    private EmpRepository empRepository;
 
-    @Test
+   /* @Test
     public void contextLoads() {
-    }
-
-    @BeforeClass
-    public void setEmpRepository () {
-        this.empRepository = empRepository;
-    }
-
+    }*/
 
     @Test
-    public void TestCreate() {
+    @DisplayName("new employee instance should be created")
+    public void createEmployee() {
         Employee employee = populateEmployeeData();
         empRepository.save(employee);
-        assertNotNull(empRepository.findById(2));
-
-
-
+        Mockito.verify(empRepository, Mockito.times(1)).save(ArgumentMatchers.any(Employee.class));
     }
+
+    @Test
+    @DisplayName("employee should be found by id")
+    public void findByIdTest() {
+        Employee employee = populateEmployeeData();
+        Mockito.when(empRepository.findById(1)).thenReturn(employee);
+        Employee actualEmployee = empRepository.findById(1);
+        assertThat(actualEmployee.getEmpId()).isEqualTo(employee.getEmpId());
+    }
+    @Test
+    @DisplayName("employee should be found by lastName, Gender and DOB")
+    public void findAllByLastNameAndGenderEnumAndBirthDateTest() {
+        Employee employee = populateEmployeeData();
+        Mockito.when(Gender.MALE.name()).thenReturn("MALE");
+        Mockito.when(employee.getBirthDate()).thenReturn(new Date("1990-11-04"));
+        Mockito.when(empRepository.findAllByLastNameAndGenderEnumAndBirthDate("kumar", Gender.MALE, new Date("1990-11-04"))).thenReturn((List<Employee>) employee);
+        List<Employee> actualEmployee = empRepository.findAllByLastNameAndGenderEnumAndBirthDate("kumar", Gender.MALE, new Date("1990-11-04"));
+        Employee emp = actualEmployee.get(0);
+        assertThat(emp.getEmpId()).isEqualTo(employee.getEmpId());
+        assertThat(emp.getLastName()).isEqualTo(employee.getLastName());
+        assertThat(emp.getGenderEnum()).isEqualTo(employee.getGenderEnum());
+        assertThat(emp.getBirthDate()).isEqualTo(employee.getBirthDate());
+    }
+
+
+
+
 
     public Employee populateEmployeeData(){
         Employee employee = new Employee();
@@ -52,14 +71,14 @@ class EmployeeManagementApplicationTests {
         Salary salary = new Salary();
 
         //employee data
-        employee.setEmpId(2);
+        employee.setEmpId(1);
         employee.setFirstName("manish");
         employee.setLastName("kumar");
         employee.setGenderEnum(Gender.MALE);
         employee.setDepartment(department);
         employee.setSalary(salary);
-        //employee.setBirthDate(LocalDate.of(1990, 11, 04));
-        //employee.setHireDate(LocalDate.of(2018, 10, 29));
+        employee.setBirthDate(new Date(1990, 11, 04));
+        employee.setHireDate(new Date(2018, 10, 29));
 
         //department data
         department.setDept_name("HR");
@@ -67,9 +86,9 @@ class EmployeeManagementApplicationTests {
 
         //salary data
         salary.setSalary(50000);
-        //salary.setFromDate(LocalDate.of(2021, 1, 01));
-        //salary.setToDate(LocalDate.of(2021, 3, 31));
-        //salary.setEmployee(employee);
+        salary.setFromDate(new Date(2021, 1, 01));
+        salary.setToDate(new Date(2021, 3, 31));
+        salary.setEmployee(employee);
 
         return employee;
     }
@@ -77,4 +96,3 @@ class EmployeeManagementApplicationTests {
 
 
 }
-*/
